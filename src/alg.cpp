0f931b15e5b6676025e76ee7b37da0ab4b5a5f93
DinoFF <124875138+NapylovaDariaAleksandrivna@users.noreply.github.com>
2023-06-03 13:36:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
 // Copyright 2022 NNTU-CS
 #include  <iostream>
+#include  <vector>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+    return tree.func(n - 1);
 }
